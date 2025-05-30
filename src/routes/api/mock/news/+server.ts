00eafa6c8f9c73089af276d04925aad4aadd0607
mattcce <ce.mattheus.cheong@gmail.com>
2025-05-30 18:47:09 +0800

    refactor: news mock route

--- src/routes/api/mock/news/+server.ts
+++ src/routes/api/mock/news/+server.ts
@@ -1,5 +1,5 @@
 import { json } from '@sveltejs/kit';
-import data from '$lib/mock/data.json';
+import data from '$lib/mock/newsdata.json';
 
 export function GET() {
 	return json(data);
--- src/routes/news/+page.ts
+++ src/routes/news/+page.ts
@@ -1,19 +1,4 @@
-interface Article {
-	id: string;
-	content: {
-		thumbnail: {
-			originalUrl: string;
-		};
-		clickThroughUrl: {
-			url: string;
-		};
-		title: string;
-		provider: {
-			displayName: string;
-		};
-		summary: string;
-	};
-}
+import type { Article } from '$lib/api/types';
 
 export async function load(): Promise<{ articles: Article[] }> {
 	const response: Response = await fetch(`http://localhost:5173/api/mock/news`, {
