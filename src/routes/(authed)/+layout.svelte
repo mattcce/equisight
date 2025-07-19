f6e4f320bde6c10e5b11bcc4979084fb976bc98d
mattcce <ce.mattheus.cheong@gmail.com>
2025-07-19 09:37:25 +0800

    refactor: bottom navbar

--- src/routes/(authed)/+layout.svelte
+++ src/routes/(authed)/+layout.svelte
@@ -3,20 +3,12 @@
 
 	import { goto } from '$app/navigation';
 	import { navigating, page } from '$app/state';
-	import NavButton from '$lib/components/NavButton.svelte';
 	import { authStore } from '$lib/states/auth.svelte';
 
+	import BottomNav from './BottomNav.svelte';
 	import TopNav from './TopNav.svelte';
 
 	const { children } = $props();
-
-	const navLocations = [
-		{ location: '/news', display: 'News', icon: '/icons/newspaper_20dp.svg' },
-		{ location: '/holdings', display: 'Holdings', icon: '/icons/account_balance_20dp.svg' },
-		{ location: '/analysis', display: 'Analysis', icon: '/icons/analytics_20dp.svg' }
-	];
-
-	const navCurrentLocation = $derived(page.url.pathname.split('/')[1]);
 
 	let isAuthenticated = $derived(authStore.isAuthenticated);
 
@@ -37,14 +29,4 @@
 	{@render children()}
 {/if}
 
-<nav class="fixed bottom-0 left-0 z-1000 w-full bg-gray-100 p-3">
-	<div class="flex h-[43px] justify-center">
-		<ul class="flex w-xs flex-row items-center justify-around">
-			{#each navLocations as { location, display, icon } (location)}
-				<li>
-					<NavButton {location} {display} {icon} currentLocation={navCurrentLocation} />
-				</li>
-			{/each}
-		</ul>
-	</div>
-</nav>
+<BottomNav />
---
+++ src/routes/(authed)/BottomNav.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import NavButton from '$lib/components/NavButton.svelte';
+
+	import { navLocations } from './utils';
+
+	const navCurrentLocation = $derived(page.url.pathname.split('/')[1]);
+</script>
+
+<nav class="fixed bottom-0 left-0 z-1000 w-full bg-gray-100 p-3">
+	<div class="flex h-[43px] justify-center">
+		<ul class="flex w-xs flex-row items-center justify-around">
+			{#each navLocations as { location, display, icon } (location)}
+				<li>
+					<NavButton {location} {display} {icon} currentLocation={navCurrentLocation} />
+				</li>
+			{/each}
+		</ul>
+	</div>
+</nav>
