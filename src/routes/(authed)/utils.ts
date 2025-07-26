4fd52e6ee512b68357a7bef5534c01bb18a528d5
mattcce <ce.mattheus.cheong@gmail.com>
2025-07-26 17:38:11 +0800

    fix: invalid icon location

--- src/routes/(authed)/utils.ts
+++ src/routes/(authed)/utils.ts
@@ -1,5 +1,5 @@
 export const navLocations = [
-	{ location: '/news', display: 'News', icon: '/icons/newspaper_20dp.svg' },
-	{ location: '/holdings', display: 'Holdings', icon: '/icons/account_balance_20dp.svg' },
-	{ location: '/analysis', display: 'Analysis', icon: '/icons/analytics_20dp.svg' }
+	{ location: '/news', display: 'News', icon: '/assets/newspaper_20dp.svg' },
+	{ location: '/holdings', display: 'Holdings', icon: '/assets/account_balance_20dp.svg' },
+	{ location: '/analysis', display: 'Analysis', icon: '/assets/analytics_20dp.svg' }
 ];
